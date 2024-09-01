f1a1a4827b24b81b6b9a4c78ef4cc35bf29c66f0
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-09-01 01:47:32 -0500

    update stock & portfolio repos

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -7,6 +7,7 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetAllStocksAsync(QueryObject query);
         Task<Stock?> GetStockByIdAsync(int id); // add ? cos we're using FirstOrDefault
+        Task<Stock?> GetStockBySymbolAsync(string symbol);
         Task<Stock> CreateStockAsync(Stock stockModel);
         Task<Stock?> UpdateStockAsync(int id, Stock stockModel);
         Task<Stock?> DeleteStockByIdAsync(int id);
